b06a3f9a007fbbc81c111a8daab95bf58ab76f41
Leandro Souza <lsouzadasilva4@gmail.com>
2025-07-18 13:52:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ with tab_home:
         st.divider()
         st.sidebar.markdown("""
         **Desenvolvido por Leandro Souza**  
-        [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/leandro-souza-bi/)
+        [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in//leandro-souza-dados/)
         [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/lsouzadasilva/streamlit_openai_langchain_read_pdf)
         """)
     sobre()
